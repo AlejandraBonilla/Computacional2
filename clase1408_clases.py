df6af8831954bf914d9b5fdba1e9b984436e6d96
AlejandraBonilla <31254521+AlejandraBonilla@users.noreply.github.com>
2018-08-14 12:59:02 -0500

    Update clase1408_clases.py

--- clase1408_clases.py
+++ clase1408_clases.py
@@ -12,3 +12,24 @@ class persona:
     def dos_m(self):
       print  ("Tengo dos manos")
       
+#######################################################################
+
+class gato:
+    
+    def __init__(self,energia,hambre):
+     self.energia=energia
+     self.hambre=hambre
+     print ("se cree un gato")
+     
+    def jugar(self):
+        if self.energia <=0 or self.hambre <=1:
+            print ("no")
+        else: 
+            self.energia -=1
+            self.hambre -=1
+            print ("Jugar")
+            
+    def extra(self):
+        self.energia +=1
+        self.hambre +=3
+        print ("extra")
