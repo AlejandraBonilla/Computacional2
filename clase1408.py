e17e28ce5d81f1fe2ba29865d53cb1e5731d5d08
AlejandraBonilla <31254521+AlejandraBonilla@users.noreply.github.com>
2018-08-14 12:35:40 -0500

    Create clase1408.py

---
+++ clase1408.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal
+"""
+
+class persona: 
+    def _init_(self):
+      print  ("Simepre se ejecuta")
+        # cada método tienen un nombre; este método se llama dos_m
+    def dos_m(self):
+      print  ("Tengo dos manos")
+      
