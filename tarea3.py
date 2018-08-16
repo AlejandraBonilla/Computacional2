67fba2910de3c3cb7f59d00791b2e143bc581cd3
AlejandraBonilla <31254521+AlejandraBonilla@users.noreply.github.com>
2018-08-16 11:41:59 -0500

    Update tarea3.py

--- tarea3.py
+++ tarea3.py
@@ -11,14 +11,7 @@ class detector:
      self.my=my
      self.masa=masa
      print ("detector activado")
-     
-    def posicion(self):
-        if self.energia <=0 or self.tiempo <=1:
-            print ("no")
-        else: 
-            self.energia -=1
-            self.hambre -=1
-            print ("Jugar")
             
-    def extra(self):
+    def cmasa(self):
+      self.cmasa=cmasa
       
