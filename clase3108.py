f99e0e3122676c4b1c5d9d99badf1fa00062d52d
AlejandraBonilla <31254521+AlejandraBonilla@users.noreply.github.com>
2018-08-31 13:26:34 -0500

    Create clase3108.py

---
+++ clase3108.py
@@ -0,0 +1,22 @@
+#librería numpy 
+
+import numpy as np 
+from numpy import matrix
+from pylab import *
+
+print (np.arange(15)) #genero un lista de los primeros 15 números : 0 a 14
+print ('---------------------------------------')
+
+r=2
+i=np.arange(n)
+j=np.arange(n)
+n=5
+a = np.zeros(n*n).reshape(n,n) # acomodo los valores en una matriz de 3 filas por 5 columnas 
+
+while r**2>=((i-n/2)**2):
+   a = np.ones()
+    
+print (a)
+
+
+imshow(a)
